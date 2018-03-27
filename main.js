a6ad718ba2d8c6a2c33e038d057f61a84f08a11e
DavideL90 <davide.lecci@icloud.com>
2018-03-27 14:44:11 +0200

    impostato ciclo con i controlli sui numeri inseriti

--- main.js
+++ main.js
@@ -5,4 +5,36 @@
 //
 // Una volta costruiti questi array, il programma dovrà generare una lista delle occorrenze.
 // Nello specifico, guardando tutti i numeri inclusi in tutti gli array, dovrà indicare quante volte compare ogni numero in tutti gli array.
-v
+
+//variabili che contengono i numeri minimo e massimo
+var min;
+var max;
+var isEqual = true;
+//Faccio un ciclo che controlla che i numeri inseriti non siano uguali. Se lo sono fossero andranno reinseriti
+do{
+  var num1 = parseInt(prompt("Inserisci un numero"));
+  while(isNaN(num1)){
+    alert("Hai inserito un valore errato! Reinseriscilo!");
+    num1 = parseInt(prompt("Inserisci un numero"));
+  }
+  var num2 = parseInt(prompt("Inserisci un numero"));
+  while(isNaN(num2)){
+    alert("Hai inserito un valore errato! Reinseriscilo!");
+    num2 = parseInt(prompt("Inserisci un numero"));
+  }
+  if(num1 < num2){
+    min = num1;
+    max = num2;
+    isEqual = false;
+  }
+  else if(num1 > num2){
+    min = num2;
+    max = num1;
+    isEqual = false;
+  }
+  else{
+    alert("Non puoi inserire valori uguali!");
+  }
+}while(isEqual)
+console.log(min);
+console.log(max);
